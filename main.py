14fed820bfe29eef225d224291f5ca72b83ea008
Ian Cazalilla Cerro <ica5181@proven.local>
2025-09-22 17:51:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+weather = st.Page("weather.py", title="Weather", icon=":material/add_circle:")
+cars = st.Page("cars.py", title="Cars", icon=":material/add_circle:")
+
+pg = st.navigation([weather,cars])
+st.set_page_config(
+    page_title="Altair | xtec.dev",
+    page_icon="𝚾",
+    initial_sidebar_state="expanded"
+)
+
+pg.run()
