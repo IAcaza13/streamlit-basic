3677cf2d4c55f3f97e416b7e6c4d0ed79755cb12
ian <iancazalillacerro@gmail.com>
2025-09-23 17:39:37 +0200

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ cars = st.Page("cars.py", title="Cars", icon=":material/add_circle:")
 disasters = st.Page("disasters.py", title="Disasters", icon = ":material/add_circle:")
 football =st.Page ("football.py", title="Football", icon = ":material/add_circle:")
 consum_de_cervesa=st.Page("consum_de_cervesa.py", title="Consumer De's Cervesa", icon = ":material/add_circle:")
-pg = st.navigation([weather,cars, disasters,football])
+pg = st.navigation([weather,cars, disasters,football,consum_de_cervesa])
 st.set_page_config(
     page_title="Altair | xtec.dev",
     page_icon="𝚾",
